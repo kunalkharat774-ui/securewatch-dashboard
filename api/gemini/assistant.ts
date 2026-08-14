9603b5af6dfc11a6b7e7b1046a97a56ede0d390d
Kunal Kharat <kunalkharat774@gmail.com>
2026-08-14 12:40:58 +0530

    update my code

--- api/gemini/assistant.ts
+++
@@ -1,7 +0,0 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default async (_req: VercelRequest, res: VercelResponse) => {
-  return res.status(410).json({
-    error: 'Osiris GPT has been removed from SecureWatch.'
-  });
-};
