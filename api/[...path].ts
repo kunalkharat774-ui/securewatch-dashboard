d3cc135ec4d59e5027b8ddfa0457eaee1f5a4ce6
Kunal Kharat <kunalkharat774@gmail.com>
2026-09-06 19:16:40 +0530

    update my code

--- api/[...path].ts
+++ api/[...path].ts
@@ -1,10 +1,7 @@
-import type express from 'express';
 import app from '../server';
 
 export const config = {
   maxDuration: 30,
 };
 
-export default function handler(req: express.Request, res: express.Response) {
-  return app(req, res);
-}
+export default app;
