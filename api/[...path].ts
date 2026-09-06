21123b54b374dcb68d06f6488539ef89b7fba5f3
Kunal Kharat <kunalkharat774@gmail.com>
2026-09-06 17:38:06 +0530

    modify code

--- api/[...path].ts
+++ api/[...path].ts
@@ -1,18 +1,10 @@
 import type express from 'express';
+import app from '../server';
 
 export const config = {
   maxDuration: 30,
 };
 
-let appPromise: Promise<typeof import('../server').default> | undefined;
-
-export default async function handler(req: express.Request, res: express.Response) {
-  try {
-    appPromise ??= import('../server').then((module) => module.default);
-    const app = await appPromise;
-    return app(req, res);
-  } catch (error) {
-    console.error('Unable to initialize SecureWatch API:', error);
-    return res.status(500).json({ error: 'Unable to initialize SecureWatch API.' });
-  }
+export default function handler(req: express.Request, res: express.Response) {
+  return app(req, res);
 }
